b1d5d9d9068c485053b6dcaeef3006262ca23092
himanshu864 <himanshu864000@gmail.com>
2024-05-01 19:21:14 +0530

    Finish Netflix Started JS

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log("Console is running...")
+console.log("Still running...")
+
+var a = prompt("Enter value of a: ")
+console.log("a: " + a)
+
+var flag = confirm("Care for a cup of virus?")
+if (flag) {
+    console.log("You're F*cked... (learn to read before clicking moron)")
+}
+else {
+    console.log("Good choice")
+}
+
+document.title = "Added using JS"
+document.body.style.background = "crimson"
