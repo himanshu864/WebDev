b8bafbd4adf477b8f999d6a2844c9cad33d075e4
himanshu864 <himanshu864000@gmail.com>
2024-05-03 19:27:16 +0530

    String

---
+++ JavaScript/String/script.js
@@ -0,0 +1,18 @@
+let x = "Himanshu";
+let y = "Malik"
+
+console.log("My name is " + x + ", and your name is " + y);
+// Template Literals
+console.log(`My name is ${x}, and your name is ${y}`);
+
+let a = "Hello lol";
+console.log(a);
+console.log(a[0]);
+console.log(a[4]);
+console.log(a[5]);
+console.log(a.length);
+console.log(a.toUpperCase());
+console.log(a.toLowerCase());
+console.log(a.slice(0, 4));
+console.log(a.replace("lo", "p"));
+console.log(a.concat("ipop"));
--- script.js
+++
@@ -1,6 +0,0 @@
-let a = "Hello";
-console.log(a);
-console.log(a[0]);
-console.log(a[4]);
-console.log(a[5]);
-console.log(a.length);
