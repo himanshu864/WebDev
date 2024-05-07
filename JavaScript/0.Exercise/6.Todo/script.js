aa3e8e8d44c9ac29f6745889d2d15ffc4ecdf60d
himanshu864 <himanshu864000@gmail.com>
2024-05-07 13:30:06 +0530

    Todo better sleepy complete WebD this week

--- JavaScript/0.Exercise/6.Todo/script.js
+++ JavaScript/0.Exercise/6.Todo/script.js
@@ -4,8 +4,22 @@ function pushtask() {
     if (enteredText.value == "")
         alert("Cmon dude");
     else {
-        let newcard = document.createElement("div");
+        let newcard = document.createElement("li");
         newcard.innerHTML = enteredText.value;
+        newcard.className = "card";
+        newcard.addEventListener("click", markpls);
         document.querySelector(".card-here").append(newcard);
     }
+    enteredText.value = "";
+}
+
+function markpls() {
+    this.classList.toggle("crossdat");
+    this.classList.toggle("criminal");
+}
+
+function election() {
+    document.querySelectorAll(".criminal").forEach(Element => {
+        Element.remove();
+    });
 }
