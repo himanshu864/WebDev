56489c9f00f7d54ed2a2f2fae333c59a0c01c756
himanshu864 <himanshu864000@gmail.com>
2024-05-23 20:42:14 +0530

    Restart Score DOM Good stuff

--- JavaScript/0.Exercise/12.TicTacToe/script.js
+++ JavaScript/0.Exercise/12.TicTacToe/script.js
@@ -1,65 +1,103 @@
+const button = document.getElementById("control-button");
 const tiles = document.querySelectorAll(".tile");
-// const scorecard = document.querySelector(".score");
+const score = document.querySelector(".score");
+const scoreP1 = document.getElementById("scoreP1");
+const scoreP2 = document.getElementById("scoreP2");
+let p1 = 0;
+let p2 = 0;
 
-let gameover = false;
 let playerOneTurn = true;
-
 let grid = [
     [-1, -1, -1],
     [-1, -1, -1],
     [-1, -1, -1]
 ];
 
+// Adds event listens to empty tiles
+const startGame = () => {
+    tiles.forEach(tile => {
+        tile.addEventListener("click", handleClick, { once: true });
+    })
+}
+
+// Start game by pressing button
+const pressButtonRestart = () => {
+    button.addEventListener("click", () => {
+        button.classList.add("hide");
+        score.classList.add("hide");
+        grid = [
+            [-1, -1, -1],
+            [-1, -1, -1],
+            [-1, -1, -1]
+        ];
+        tiles.forEach(tile => {
+            tile.innerText = "";
+            tile.style.backgroundColor = "lavender";
+        });
+        playerOneTurn = true;
+        startGame();
+    })
+}
+
+// Restart Logic
+const restartButton = () => {
+    button.innerText = "Play Again";
+    button.classList.remove("hide");
+    score.classList.remove("hide");
+    pressButtonRestart();
+}
+
 // Modified grid based on player
-function handleClick(e) {
+const handleClick = (e) => {
     const cell = e.target;
     const idx = Array.from(tiles).indexOf(cell);
     const row = Math.floor(idx / 3);
     const col = idx % 3;
-    console.log(row, col);
 
     if (playerOneTurn) {
-        cell.innerHTML = "X";
+        cell.innerText = "X";
         playerOneTurn = false;
         grid[row][col] = 1;
     }
     else {
-        cell.innerHTML = "O";
+        cell.innerText = "O";
         playerOneTurn = true;
         grid[row][col] = 0;
     }
-    // cell.style.backgroundColor = "blueviolet";
+    cell.style.backgroundColor = "blueviolet";
 
     areYouWinningSon();
 }
 
-// Adds event listens to empty tiles
-tiles.forEach(tile => {
-    tile.addEventListener("click", handleClick, { once: true });
-})
-
 // Logic for gameover
-function areYouWinningSon() {
+const areYouWinningSon = () => {
     const result = scoreMate();
 
     if (result != -1) {
         if (result == 1) {
-            alert("Player One Wins!");
+            score.innerText = "You win, Player One!";
+            p1++;
+            scoreP1.innerText = p1;
         }
         if (result == 0) {
-            alert("Player Two Wins!");
+            score.innerText = "You win, Player Two!";
+            p2++;
+            scoreP2.innerText = p2;
         }
         if (result == 2) {
-            alert("It's a draw!");
+            score.innerText = "It's a draw!";
         }
-        gameover = true;
+
+        // removes tiles click event onces game over and adds button
+        restartButton();
         tiles.forEach(tile => {
             tile.removeEventListener("click", handleClick);
-        })
+        });
     }
 }
 
-function scoreMate() {
+// Checks Game status
+const scoreMate = () => {
     if (linecheck(1))
         return 1; // Player 1 wins
     if (linecheck(0))
@@ -74,27 +112,34 @@ function scoreMate() {
 }
 
 // Flag every line for each player
-function linecheck(x) {
-    let diagLR = true, diagRL = true;
+const linecheck = (x) => {
+    let diagLR = true;
+    let diagRL = true;
 
     for (let i = 0; i < 3; i++) {
-        let hori = true, verti = true;
+        let hori = true;
+        let verti = true;
 
         for (let j = 0; j < 3; j++) {
             if (grid[i][j] != x)
                 hori = false;
+
             if (grid[j][i] != x)
                 verti = false;
         }
+
         if (hori || verti)
             return true;
 
         if (grid[i][i] != x)
             diagLR = false;
+
         if (grid[i][2 - i] != x)
             diagRL = false;
     }
-    if (diagLR || diagRL)
-        return true;
-    return false;
+
+    return diagLR || diagRL;
 }
+
+// START GAME
+pressButtonRestart();
