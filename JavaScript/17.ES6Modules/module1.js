0434c3743e326f0625ae37258ba30ad7a137e698
himanshu864 <himanshu864000@gmail.com>
2024-05-26 23:16:12 +0530

    ES6 Modules

---
+++ JavaScript/17.ES6Modules/module1.js
@@ -0,0 +1,5 @@
+export function clickKrdoPls(button, text) {
+    button.addEventListener("click", () => {
+        console.log(text);
+    });
+}
---
+++ JavaScript/17.ES6Modules/script.js
@@ -0,0 +1,6 @@
+import { clickKrdoPls } from "./module1.js";
+
+const button = document.querySelector("#mokshKaButton");
+let grid = "Maine click kara";
+
+clickKrdoPls(button, grid);
