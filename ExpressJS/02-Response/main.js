86f150ed3d1c3708718a6f0c12fd7e428e6bcfce
himanshu864 <himanshu864000@gmail.com>
2024-06-10 22:02:29 +0530

    Express route :

--- ExpressJS/02-Response/main.js
+++ ExpressJS/02-Response/main.js
@@ -2,15 +2,22 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// Express routing
+const birds = require('./birds')
+
+app.use('/birds', birds)
+
 app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-    console.log("You a get request!");
-    res.send('Hello World!')
-}).post('/', (req, res) => {
-    console.log("You a post request!");
-    res.send('Hello World post!')
-}) // same of put and delete
+app.route('/')
+    .get((req, res) => {
+        console.log("You a get request!");
+        res.send('Hello World!')
+    })
+    .post((req, res) => {
+        console.log("You a post request!");
+        res.send('Hello World post!')
+    })
 
 app.get('/index', (req, res) => {
     console.log("this is a index");
