5bdd9a7a573be18acaaa03b21a4090b255c7c824
himanshu864 <himanshu864000@gmail.com>
2024-06-12 18:18:55 +0530

    JWT .env User authentication

--- JWT/server.js
+++ JWT/server.js
@@ -1,6 +1,12 @@
+require('dotenv').config();
+
 const express = require("express");
 const app = express();
-const port = 3000;
+
+const jwt = require("jsonwebtoken");
+
+// use json inside request's body
+app.use(express.json());
 
 const posts = [
     {
@@ -13,14 +19,35 @@ const posts = [
     }
 ];
 
-app.get('/posts', (req, res) => {
-    res.json(posts);
+app.get('/posts', authenticateToken, (req, res) => {
+    // send a header authenication : Bearer <TOKEN> in postman
+    res.json(posts.filter(post => post.username === req.user.name));
 })
 
-app.get('/login', (req, res) => {
-    // authenticate
+app.post('/login', (req, res) => {
+    // Assuming User Authentication using Node and bcrypt
+
+    const username = req.body.username;
+    const user = { name: username };
+
+    // (string to serialize, secret key)
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+
+    res.json({ accesstoken: accessToken });
 })
 
-app.listen(port, () => {
-    console.log("Connected!");
-});
+function authenticateToken(req, res, next) {
+    // get token, verify correct user and display user
+
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if (!token) return res.sendStatus(401);
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403);
+        req.user = user;
+        next();
+    })
+}
+
+app.listen(3000);
