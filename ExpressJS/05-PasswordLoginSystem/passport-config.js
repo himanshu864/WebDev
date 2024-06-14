30b14b2a78d33b789be3cfa685a902b59c02d65d
himanshu864 <himanshu96864@gmail.com>
2024-06-14 17:33:14 +0530

    Password Login System 25%

---
+++ ExpressJS/05-PasswordLoginSystem/passport-config.js
@@ -0,0 +1,5 @@
+const localStrategy = require('passport-local').Strategy;
+
+function initialize(passport) {
+    // I dont get it
+}
---
+++ ExpressJS/05-PasswordLoginSystem/server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+const bcrypt = require("bcrypt")
+
+const users = [];
+
+app.set('view-engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+
+// const passport = require('passport');
+// const initializePassport = require('./passport-config.js')
+// initializePassport(passport);
+
+app.get('/', (req, res) => {
+    res.render('index.ejs', { user: "Himanshu" });
+});
+
+app.get('/login', (req, res) => {
+    res.render('login.ejs');
+});
+
+app.get('/register', (req, res) => {
+    res.render('register.ejs');
+});
+
+app.post('/register', async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+        });
+        res.redirect('/login');
+    } catch {
+        console.log("bruh");
+        res.redirect('/register');
+    }
+    console.log(users);
+})
+
+app.listen(3000);
