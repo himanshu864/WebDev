d1ac3a1c309559f326d9b48a7b3ee53d55301be0
himanshu864 <himanshu96864@gmail.com>
2024-06-16 12:37:55 +0530

    http headers; statusCode

--- NodeJS/06-RESTful API/server.js
+++ NodeJS/06-RESTful API/server.js
@@ -24,14 +24,20 @@ app.get('/users', (req, res) => {
 // returns JSON for client side rendering
 app.route('/api/users')
     .get((req, res) => {
+        console.log(req.headers);
+        res.setHeader("X-custom-header", "Himanshu A");
         return res.json(users);
     })
     .post((req, res) => {
         // Create user
         const user = req.body;
+
+        if (!user || !user.first_name || !user.last_name || !user.email || !user.gender || !user.job_title)
+            return res.status(404).json({ Error: "All fields are required!" });
+
         users.push({ id: users.length + 1, ...user });
         fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
-            return res.json({ status: "Success", id: users.length });
+            return res.status(201).json({ status: "Success", id: users.length });
         })
     })
 
@@ -42,16 +48,16 @@ app.route('/api/users/:id')
         const id = Number(req.params.id);
         const user = users.find(user => user.id === id);
         if (!user)
-            return res.json({ status: "404 : Not found" });
+            return res.status(404).json({ status: "User doesn't exist!" });
         return res.json(user);
     })
     .patch((req, res) => {
-        // Update user
+        // Update users
         const id = Number(req.params.id);
         const updates = req.body;
         const userIdx = users.findIndex(user => user.id === id);
         if (userIdx === -1)
-            return res.json({ status: "Not found..." });
+            return res.status(404).json({ status: "User doesn't exist!" });
 
         users[userIdx] = { ...users[userIdx], ...updates }
         fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
@@ -60,11 +66,12 @@ app.route('/api/users/:id')
         return res.json({ status: "Success..." });
     })
     .delete((req, res) => {
+        // delete user
         const id = Number(req.params.id);
 
         const userIdx = users.findIndex(user => user.id === id);
         if (userIdx === -1)
-            return res.json({ status: "Not found..." });
+            return res.status(404).json({ status: "User doesn't exist!" });
 
         const updated = users.filter(user => user.id !== id);
         fs.writeFile("./MOCK_DATA.json", JSON.stringify(updated), (err, data) => {
