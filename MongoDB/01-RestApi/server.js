07599f124690791604bd681f371ae6e8a5d256fe
himanshu864 <himanshu96864@gmail.com>
2024-06-16 21:27:42 +0530

    MongoDB Connect CRUD

--- MongoDB/01-RestApi/server.js
+++ MongoDB/01-RestApi/server.js
@@ -1,79 +1,74 @@
 const express = require("express");
 const app = express();
-const users = require("./MOCK_DATA.json");
-const fs = require("fs");
-const path = require('path');
+const mongoose = require('mongoose');
 
-// Middleware to parse URL-encoded payloads into req.body
+// Connect
+mongoose.connect('mongodb://127.0.0.1:27017/restUsers')
+    .then(() => console.log("MongoDB Connected!"))
+    .catch(err => console.log("MongoDB Error : ", err));
+
+// Schema
+const userSchema = new mongoose.Schema({
+    first_name: { type: String, required: true },
+    last_name: { type: String },
+    email: { type: String, required: true, unique: true },
+    gender: { type: String },
+    job_title: { type: String }
+},
+    { timestamps: true }
+);
+
+// Model
+const User = mongoose.model('user', userSchema);
+
 app.use(express.urlencoded({ extended: false }));
 
-// Serve HTML with user list (SSR)
-app.get('/users', (req, res) => {
+app.get('/users', async (req, res) => {
+    const allDbUsers = await User.find({});
     const html =
         `<ul>
-            ${users.map(user => `<li> ${user.first_name} </li>`).join("")}
+            ${allDbUsers.map(user => `<li> ${user.first_name} - ${user.email}</li>`).join("")}
         </ul>`
     return res.send(html);
 })
 
-// Utility function for updating MOCK_DATA.JSON file
-const writeToFile = (data, res, msg) => {
-    const filePath = path.join(__dirname, './MOCK_DATA.json');
-    fs.writeFile(filePath, JSON.stringify(data), (err) => {
-        if (err)
-            return res.status(500).json({ status: "Error", message: "Failed to write to file" });
-        else
-            return res.status(201).json({ status: "Success", message: msg });
-    })
-}
-
-// Middleware to validate users existence
-app.use('/api/users/:id', (req, res, next) => {
-    const userId = Number(req.params.id);
-    const userIdx = users.findIndex(user => user.id === userId);
-    if (userIdx === -1)
-        return res.status(404).json({ status: "User doesn't exist!" });
-    next();
-})
-
-// REST API
-// Route to handle user-related API requests
 app.route('/api/users')
-    .get((req, res) => {
+    .get(async (req, res) => {
+        const users = await User.find({});
         return res.json(users);
     })
-    .post((req, res) => {
+    .post(async (req, res) => {
         const user = req.body;
         const requiredFields = ['first_name', 'last_name', 'email', 'gender', 'job_title'];
 
-        // Validate all fields
         for (const field of requiredFields)
             if (!user[field])
-                return res.status(404).json({ Error: "All fields are required!" });
+                return res.status(400).json({ Error: "All fields are required!" });
 
         // Create user
-        users.push({ id: users.length + 1, ...user });
-        writeToFile(users, res, "User created Successfully!");
+        await User.create({ ...user });
+        return res.status(201).json({ status: "Successfully Created!" });
     })
 
-// Route to handle dynamic user API requests
 app.route('/api/users/:id')
-    .get((req, res) => {
-        const userId = Number(req.params.id);
-        const user = users.find(user => user.id === userId);
+    .all(async (req, res, next) => {
+        const user = await User.findById(req.params.id);
+        if (!user)
+            return res.status(404).json({ error: "User not found" });
+        next();
+    })
+    .get(async (req, res) => {
+        const user = await User.findById(req.params.id);
         return res.json(user);
     })
-    .patch((req, res) => {
-        const userId = Number(req.params.id);
+    .patch(async (req, res) => {
         const updates = req.body;
-        const userIdx = users.findIndex(u => u.id === userId);
-        users[userIdx] = { ...users[userIdx], ...updates };
-        writeToFile(users, res, "User updated Successfully!");
+        await User.findByIdAndUpdate(req.params.id, { ...updates });
+        return res.json({ status: "Successfully Updated!", updates: { ...updates } });
     })
-    .delete((req, res) => {
-        const userId = Number(req.params.id);
-        const updatedUsers = users.filter(user => user.id !== userId);
-        writeToFile(updatedUsers, res, "User deleted Successfully!");
+    .delete(async (req, res) => {
+        await User.findByIdAndDelete(req.params.id);
+        return res.json({ status: "Successfully Deleted!" });
     })
 
 app.listen(3000);
