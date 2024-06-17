4b6da51907b7879f4a23d123b09cff77023d2d60
himanshu864 <himanshu96864@gmail.com>
2024-06-17 12:37:32 +0530

    Debug Trycatch; Global Later

--- MongoDB/01-RestApi/server.js
+++ MongoDB/01-RestApi/server.js
@@ -5,16 +5,17 @@ const mongoose = require('mongoose');
 // Connect
 mongoose.connect('mongodb://127.0.0.1:27017/restUsers')
     .then(() => console.log("MongoDB Connected!"))
-    .catch(err => console.log("MongoDB Error : ", err));
+    .catch(err => { console.log("MongoDB Error : ", err), process.exit(1) });
 
 // Schema
-const userSchema = new mongoose.Schema({
-    first_name: { type: String, required: true },
-    last_name: { type: String },
-    email: { type: String, required: true, unique: true },
-    gender: { type: String },
-    job_title: { type: String }
-},
+const userSchema = new mongoose.Schema(
+    {
+        first_name: { type: String, required: true },
+        last_name: { type: String },
+        email: { type: String, required: true, unique: true },
+        gender: { type: String },
+        job_title: { type: String }
+    },
     { timestamps: true }
 );
 
@@ -22,6 +23,25 @@ const userSchema = new mongoose.Schema({
 const User = mongoose.model('user', userSchema);
 
 app.use(express.urlencoded({ extended: false }));
+
+// Global Middleware
+app.use((req, res, next) => {
+    if (mongoose.connection.readyState !== 1) // 1 means connected
+        return res.status(500).json({ error: "Database Connection Problem" });
+
+    try {
+        User.find({}); // Check if Model connected
+        next();
+    } catch (error) {
+        res.status(500).json({ error: "Database Connection Problem" })
+    }
+})
+
+// // Global error handler middleware
+// app.use((err, req, res, next) => {
+//     console.error(err.stack);
+//     res.status(500).send('Something went wrong!');
+// });
 
 app.get('/users', async (req, res) => {
     const allDbUsers = await User.find({});
@@ -46,15 +66,25 @@ app.route('/api/users')
                 return res.status(400).json({ Error: "All fields are required!" });
 
         // Create user
-        await User.create({ ...user });
-        return res.status(201).json({ status: "Successfully Created!" });
+        try {
+            await User.create({ ...user });
+            return res.status(201).json({ status: "Successfully Created!" });
+        } catch {
+            return res.status(400).json({ Error: "Invalid Fields Format" });
+        }
     })
 
 app.route('/api/users/:id')
     .all(async (req, res, next) => {
-        const user = await User.findById(req.params.id);
+        const userId = req.params.id;
+
+        if (!mongoose.Types.ObjectId.isValid(userId))
+            return res.status(404).json({ error: "Invalid UserID Format" });
+
+        const user = await User.findById(userId);
         if (!user)
             return res.status(404).json({ error: "User not found" });
+
         next();
     })
     .get(async (req, res) => {
@@ -63,12 +93,20 @@ app.route('/api/users/:id')
     })
     .patch(async (req, res) => {
         const updates = req.body;
-        await User.findByIdAndUpdate(req.params.id, { ...updates });
-        return res.json({ status: "Successfully Updated!", updates: { ...updates } });
+        try {
+            await User.findByIdAndUpdate(req.params.id, { ...updates });
+            return res.json({ status: "Successfully Updated!", updates: { ...updates } });
+        } catch {
+            return res.status(500).json({ error: "Something Went Wrong" });
+        }
     })
     .delete(async (req, res) => {
-        await User.findByIdAndDelete(req.params.id);
-        return res.json({ status: "Successfully Deleted!" });
+        try {
+            await User.findByIdAndDelete(req.params.id);
+            return res.json({ status: "Successfully Deleted!" });
+        } catch {
+            return res.status(500).json({ error: "Something Went Wrong" });
+        }
     })
 
 app.listen(3000);
