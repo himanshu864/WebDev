b4249871ec46560973d2b1860759ceb5d2e31d97
himanshu864 <himanshu96864@gmail.com>
2024-06-29 16:26:44 +0530

    feat: express-session documentation almost passport

--- ExpressJS/05-PasswordLoginSystem/passport-config.js
+++ ExpressJS/05-PasswordLoginSystem/passport-config.js
@@ -1,5 +1,9 @@
-const localStrategy = require('passport-local').Strategy;
+const localStrategy = require("passport-local").Strategy;
 
 function initialize(passport) {
-    // I dont get it
+  // I dont get it
 }
+
+// const passport = require('passport');
+// const initializePassport = require('./passport-config.js')
+// initializePassport(passport);
--- ExpressJS/05-PasswordLoginSystem/server.js
+++ ExpressJS/05-PasswordLoginSystem/server.js
@@ -1,43 +1,78 @@
 const express = require("express");
 const app = express();
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcrypt");
+const session = require("express-session");
 
+// Example Database
 const users = [];
 
-app.set('view-engine', 'ejs');
+app.set("view-engine", "ejs");
+app.set("views", __dirname + "/views");
+
 app.use(express.urlencoded({ extended: false }));
+app.use(
+  session({
+    resave: false, // don't save session if unmodified
+    saveUninitialized: false, // don't create session until something stored
+    secret: "shhhh, very secret",
+    cookie: {
+      maxAge: 60000, // session lasts 1 minute
+    },
+  })
+);
 
-// const passport = require('passport');
-// const initializePassport = require('./passport-config.js')
-// initializePassport(passport);
-
-app.get('/', (req, res) => {
-    res.render('index.ejs', { user: "Himanshu" });
+// Static routes
+app.get("/", (req, res) => {
+  if (!req.session.user) return res.redirect("/login");
+  res.render("index.ejs", { user: req.session.user.name });
 });
 
-app.get('/login', (req, res) => {
-    res.render('login.ejs');
+app.get("/login", (req, res) => {
+  if (req.session.user) return res.redirect("/");
+  res.render("login.ejs");
 });
 
-app.get('/register', (req, res) => {
-    res.render('register.ejs');
+app.get("/register", (req, res) => {
+  if (req.session.user) return res.redirect("/");
+  res.render("register.ejs");
 });
 
-app.post('/register', async (req, res) => {
-    try {
-        const hashedPassword = await bcrypt.hash(req.body.password, 10);
-        users.push({
-            id: Date.now().toString(),
-            name: req.body.name,
-            email: req.body.email,
-            password: hashedPassword
-        });
-        res.redirect('/login');
-    } catch {
-        console.log("bruh");
-        res.redirect('/register');
-    }
-    console.log(users);
-})
+// POST routes
+app.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  users[email] = {
+    id: Date.now().toString(),
+    name,
+    email,
+    password: hashedPassword,
+  };
+  return res.render("login.ejs", {
+    msg: `<div class="success">User successfully created</div> `,
+  });
+});
+
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  const user = users[email];
+
+  if (!user)
+    return res.render("login.ejs", {
+      msg: `<div class="error">Username doesn't exist</div>`,
+    });
+
+  if (!bcrypt.compareSync(password, user.password))
+    return res.render("login.ejs", {
+      msg: `<div class="error">Invalid Password</div> `,
+    });
+
+  req.session.user = user;
+  res.redirect("/");
+});
+
+app.post("/logout", (req, res) => {
+  req.session.destroy();
+  res.redirect("/login");
+});
 
 app.listen(3000);
