5733a70404fb5dd66c9a57a5ae45fa1860a7e4d3
himanshu864 <himanshu96864@gmail.com>
2024-07-01 17:46:51 +0530

    feat: Implement Payment Gateway Razerpay

---
+++ ExpressJS/00-Projects/01-eCommerce-Callback/app.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+app.set("view-engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const { createOrder } = require("./routes/order.js");
+
+const items = [
+  {
+    name: "Sony WH-1000XM4",
+    price: 19990,
+    image: "https://m.media-amazon.com/images/I/51DkbWZIg+L._SL1500_.jpg",
+    alt: "xm4",
+  },
+  {
+    name: "RK ROYAL KLUDGE RK68",
+    price: 12081,
+    image: "https://m.media-amazon.com/images/I/61MbLfXHInS._SL1500_.jpg",
+    alt: "rk68",
+  },
+  {
+    name: "Spring Snow by Yukio Mishima",
+    price: 361,
+    image: "https://m.media-amazon.com/images/I/710B2qTOD6L._SL1500_.jpg",
+    alt: "mishima",
+  },
+];
+
+app.get("/", (req, res) => {
+  return res.render("home.ejs", { items });
+});
+
+app.post("/createOrder", createOrder);
+
+app.listen(3000);
---
+++ ExpressJS/00-Projects/01-eCommerce-Callback/routes/order.js
@@ -0,0 +1,42 @@
+const Razorpay = require("razorpay");
+const instance = new Razorpay({
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_KEY_SECRET,
+});
+
+const createOrder = (req, res) => {
+  const { name, price, image } = req.body;
+  const amount = price * 100;
+
+  const options = {
+    amount: amount,
+    currency: "INR",
+    receipt: `order_rcptid_${Date.now()}`, // unique receipt everytime
+  };
+
+  // create order
+  instance.orders.create(options, function (err, order) {
+    if (err) {
+      return res.send(500).json({
+        success: false,
+        msg: "Order creation failed",
+        error: err,
+      });
+    }
+    res.send({
+      success: true,
+      msg: "Order Created",
+      order_id: order.id,
+      amount: amount,
+      key_id: process.env.RAZORPAY_KEY_ID,
+      product_name: name,
+      description: name,
+      image: image,
+      contact: "8567345632",
+      name: "Gaurav Kumar",
+      email: "gauravKumar123@gmail.com",
+    });
+  });
+};
+
+module.exports = { createOrder };
