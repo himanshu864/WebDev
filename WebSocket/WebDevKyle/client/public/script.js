414ef5b14e7d5b81a186b65893a59dede4f7649c
himanshu864 <himanshu864000@gmail.com>
2025-03-16 14:15:46 +0530

    chat + room messaging; public + private

--- WebSocket/WebDevKyle/client/public/script.js
+++ WebSocket/WebDevKyle/client/public/script.js
@@ -5,7 +5,9 @@ const messagesList = document.getElementById("messages-list");
 const messageForm = document.getElementById("message-form");
 const roomForm = document.getElementById("room-form");
 const messageInput = document.getElementById("message-input");
+const privateRoomInput = document.getElementById("private-room");
 const roomInput = document.getElementById("room-input");
+const roomLeaveButton = document.getElementById("room-leave-button");
 
 const appendMessage = (msg) => {
   const item = document.createElement("li");
@@ -14,20 +16,58 @@ const appendMessage = (msg) => {
   window.scrollTo(0, document.body.scrollHeight);
 };
 
+// On connection
+const username = prompt("");
+socket.on("connect", () => {
+  appendMessage(`You connected! Socket ID: ${socket.id}`);
+  socket.emit("new-user", username);
+});
+
+// On Message
 messageForm.addEventListener("submit", (event) => {
   event.preventDefault();
-  if (!messageInput) return;
+  const message = messageInput.value;
+  const privateRoom = privateRoomInput.value;
+  if (!message) return;
 
-  appendMessage(`You: ${messageInput.value}`);
+  socket.emit("new-message", message, privateRoom);
+  appendMessage(`You: ${message}`);
   messageInput.value = "";
   messageInput.focus();
 });
 
+// On Room Join
 roomForm.addEventListener("submit", (event) => {
   event.preventDefault();
   const room = roomInput.value;
   if (!room) return;
 
+  socket.emit("join-room", room, (msg) => appendMessage(msg));
   roomInput.value = "";
-  roomInput.focus();
+});
+
+// On Room Leave
+roomLeaveButton.addEventListener("click", (event) => {
+  const room = roomInput.value;
+  if (!room) return;
+
+  socket.emit("leave-room", room, (msg) => appendMessage(msg));
+  roomInput.value = "";
+});
+
+// Listeners
+socket.on("user-connect", (name) => {
+  appendMessage(`${name} connected!`);
+});
+
+socket.on("chat-message", (name, msg) => {
+  appendMessage(`${name}: ${msg}`);
+});
+
+socket.on("room-message", (name, room, msg) => {
+  appendMessage(`${name}(${room}): ${msg}`);
+});
+
+socket.on("user-disconnect", (name) => {
+  appendMessage(`${name} disconnected!`);
 });
--- WebSocket/WebDevKyle/server/server.js
+++ WebSocket/WebDevKyle/server/server.js
@@ -4,10 +4,33 @@ const io = require("socket.io")(3000, {
   },
 });
 
+const user = {}; // map[socket.id] = name
+
 io.on("connection", (socket) => {
-  console.log("User connected!");
+  socket.on("new-user", (name) => {
+    user[socket.id] = name;
+    socket.broadcast.emit("user-connect", name);
+  });
+
+  socket.on("new-message", (msg, room) => {
+    if (!room) socket.broadcast.emit("chat-message", user[socket.id], msg);
+    else socket.to(room).emit("room-message", user[socket.id], room, msg);
+  });
+
+  socket.on("join-room", (room, cb) => {
+    socket.join(room);
+    cb(`You joined a Room with ID : ${room}`);
+    socket.to(room).emit("room-message", user[socket.id], room, "Joined");
+  });
+
+  socket.on("leave-room", (room, cb) => {
+    socket.leave(room);
+    cb(`You left Room with ID : ${room}`);
+    socket.to(room).emit("room-message", user[socket.id], room, "Left");
+  });
 
   socket.on("disconnect", () => {
-    console.log("User disconnected!");
+    socket.broadcast.emit("user-disconnect", user[socket.id]);
+    delete user[socket.id];
   });
 });
